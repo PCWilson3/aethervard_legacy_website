d747c0c1887e0db693b1fcb9c56659a6da4e88e9
Parker Wilson <parker@tract.us>
2022-11-04 19:54:37 -0600

    Set up react code.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Parker Wilson</h1>
+      <h2>Name of second developer: ________</h2>
+    </div>
+  );
+}
+
+export default App;
